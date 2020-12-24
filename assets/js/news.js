d64b979bb82a9c04d1e1c016c27b00596d532afb
David Daly <dalyd14@gmail.com>
2020-12-24 00:21:11 -0500

    made slight changes to news

--- assets/js/news.js
+++ assets/js/news.js
@@ -5,23 +5,15 @@ var newsArticles = [
         link: 'https://bizjournals.com/triad/news/2020/12/11/focus-on-guilford-apartment-boom.html',
         websiteName: 'bizjournals.com',
         image: 'https://media.bizj.us/view/img/11697467/8-west*xx1499-1124-251-0.jpg',
-        date: 'Dec 11, 2020, 6:00 am EST'
+        date: 'Dec 11, 2020'
     },
     {
-        title: "Focus On Guilford Apartment Boom",
-        description: "Apartment boom forecasts economic and population expansion",
-        link: 'https://bizjournals.com/triad/news/2020/12/11/focus-on-guilford-apartment-boom.html',
-        websiteName: 'bizjournals.com',
-        image: 'https://media.bizj.us/view/img/11697467/8-west*xx1499-1124-251-0.jpg',
-        date: 'Dec 11, 2020, 6:00 am EST'
-    },
-    {
-        title: "Focus On Guilford Apartment Boom",
-        description: "Apartment boom forecasts economic and population expansion",
-        link: 'https://bizjournals.com/triad/news/2020/12/11/focus-on-guilford-apartment-boom.html',
-        websiteName: 'bizjournals.com',
-        image: 'https://media.bizj.us/view/img/11697467/8-west*xx1499-1124-251-0.jpg',
-        date: 'Dec 11, 2020, 6:00 am EST'
+        title: "Unionville powers past Kennett, 45-7",
+        description: "Indians’ explosive offense continues to produce on the ground and in the air",
+        link: 'https://www.unionvilletimes.com/?p=18567',
+        websiteName: 'unionvilletimes.com',
+        image: 'https://www.unionvilletimes.com/wp-content/uploads/2013/10/KHSUHS2013-4.jpg',
+        date: 'Oct 5th, 2013'
     }
 ]
 var ourNews = []
@@ -36,14 +28,16 @@ $(document).ready(function() {
                         <div class="article-img">
                             <img src=${article.image}>
                         </div>
-                        <div class="article-title">
-                            <h3>${article.title}</h3>
+                        <div class="article-content-info">
+                            <div class="article-title">
+                                <h3>${article.title}</h3>
+                            </div>                        
+                            <div class="article-preview">
+                                <p>${article.description}</p>
+                            </div>                        
                         </div>
                     </div>
                     <div class="article-sub-info">
-                        <div class="article-preview">
-                            <p>${article.description}</p>
-                        </div>
                         <div class="article-date">
                             <h6>${article.date}</h6>
                         </div>
