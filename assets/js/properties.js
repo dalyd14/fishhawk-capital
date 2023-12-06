27d2cbd8ced37e333d1e8f0bcfb872288e93860c
dalyd14 <dalyd14@gmail.com>
2023-12-06 16:18:33 -0500

    switched up the pictures

--- assets/js/properties.js
+++ assets/js/properties.js
@@ -60,15 +60,14 @@ var props = [
         size: '12 Units',
         about: "Plaza One 89 is a 42-bed (12 units) Class A community at Clemson University in Clemson, SC. Perfectly positioned adjacent to campus, Plaza One 89 is located less than 0.3 miles from Memorial Stadium and 0.1 miles from downtown Clemson, home to numerous restaurants, bars and boutique shops.",
         images: [
+            'https://www.plazaone89.com/wp-content/uploads/2018/10/IMG-0016.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_5194-1024x768.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_6015-1024x768.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_4225-1024x768.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_7469-1024x768.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_2040-1024x768.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_8949-1024x768.jpg',
-            'https://www.plazaone89.com/wp-content/uploads/2019/08/IMG_2068-768x1024.jpg',
             'https://www.plazaone89.com/wp-content/uploads/2018/10/IMG-0003-1-1024x768.jpg',
-            'https://www.plazaone89.com/wp-content/uploads/2018/10/IMG-0012-1-768x1024.jpg'
         ],
         embedCode: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5216.082624532959!2d-82.84305122301186!3d34.68304048415821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88585e42f93527bb%3A0xa3a9f15a6987b63c!2sPlaza%20One%2089!5e1!3m2!1sen!2sus!4v1701897206903!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     }
